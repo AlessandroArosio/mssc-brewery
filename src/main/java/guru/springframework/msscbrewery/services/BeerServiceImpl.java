3b48078af9cf7701d49b3d5c883e0913b9584d25
Alessandro Arosio <alessandro.arosio1@gmail.com>
2019-10-10 18:52:21 +0100

    Added service, controller and dto

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+  @Override
+  public BeerDto getBeerById(UUID beerId) {
+    return BeerDto.builder()
+        .id(UUID.randomUUID())
+        .beerName("London Pale Ale")
+        .beerStyle("Pale Ale")
+        .build();
+  }
+}
